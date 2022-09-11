02b1f761610e7874c35acccd697d8f542a778854
herkerz <hkerz@itba.edu.ar>
2022-09-11 01:23:44 -0300

    surface update

--- src/zulip/surface.py
+++ src/zulip/surface.py
@@ -2,58 +2,63 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
 
+def plot_surface(url: str, max_depth: int):
+    df = pd.read_table(url,parse_dates=["fecha"])
+    df = df[df["maxdepth"] == max_depth]
+    
+    x = df["minsplit"]
+    y = df["minbucket"]
+    z = df["ganancia"]
+    
+    ax = plt.axes(projection='3d')
 
-url = "https://raw.githubusercontent.com/herkerz/labo/main/src/zulip/data/baye_max_depth_7_8_zoom.txt"
-df = pd.read_table(url,parse_dates=["fecha"])
+    ax.plot_trisurf(x, y, z,
+                    cmap="viridis",
+                    alpha=0.75)
 
-# df_7 = df[df.maxdepth==7]
+    ax.set_title(f'Ganancia Optimizacion Bayesiana Max Depth {max_depth}')
+    ax.set_xlabel("minbucket")
+    ax.set_ylabel("minsplit")
+    ax.set_zlabel("Ganancia")
+    plt.show()   
 
-# x7 = df_7["minbucket"]
-# y7= df_7["minsplit"]
-# z7 = df_7["ganancia"]
+def compare_surface(urls: list, max_depths: list):
+    ax = plt.axes(projection='3d')
+    
+    for url, max_depth in zip(urls,max_depths):
+        
+        df = pd.read_table(url,parse_dates=["fecha"])
+        df = df[df["maxdepth"] == max_depth]
+        x = df["minsplit"]
+        y = df["minbucket"]
+        z = df["ganancia"]
+        
+        surf = ax.plot_trisurf(x, y, z,
+                        alpha=0.7,
+                        label=f"Max Depth {max_depth}")
+        surf._facecolors2d = surf._facecolor3d
+        surf._edgecolors2d = surf._edgecolor3d
+        ax.legend()
+        
+    ax.set_title(f'Ganancia Optimizacion Bayesiana Max Depth {max_depth}')
+    ax.set_xlabel("minbucket")
+    ax.set_ylabel("minsplit")
+    ax.set_zlabel("Ganancia")
+    plt.show()   
+        
+        
+    
+url = "https://raw.githubusercontent.com/herkerz/labo/main/src/zulip/data/baye_max_depth_7_8.txt"
 
-# df_8 = df[df.maxdepth==8]
-
-# x8 = df_8["minbucket"]
-# y8= df_8["minsplit"]
-# z8 = df_8["ganancia"]
-
-
-
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-
-# #ax.plot_trisurf(x7, y7, z7,cmap="spring",alpha=0.73)
-# ax.plot_trisurf(x8, y8, z8,cmap="viridis",alpha=0.75)
-
-# ax.set_title('Ganancia Optimizacion Bayesiana')
-# ax.set_xlabel("minbucket")
-# ax.set_ylabel("minsplit")
-# ax.set_zlabel("ganancia")
-# plt.show()
+# plot_surface(url, 8)
 
 url_zoom = "https://raw.githubusercontent.com/herkerz/labo/main/src/zulip/data/baye_max_depth_7_8_zoom.txt"
 
-df_zoom = pd.read_table(url_zoom,parse_dates=["fecha"])
-
-df_8z = df_zoom[df_zoom.maxdepth==8]
-
-x8_zoom  = df_8z["minbucket"]
-y8_zoom = df_8z["minsplit"]
-z8_zoom = df_8z["ganancia"]
+# plot_surface(url_zoom, 8)
 
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+url_14= "https://raw.githubusercontent.com/herkerz/labo/main/src/zulip/data/baye_max_depth_13_14.txt"
 
-#ax.plot_trisurf(x7, y7, z7,cmap="spring",alpha=0.73)
-ax.plot_trisurf(x8_zoom, y8_zoom, z8_zoom,cmap="viridis",alpha=0.75)
+# plot_surface(url_14, 14)
 
-ax.set_title('Ganancia Optimizacion Bayesiana')
-ax.set_xlabel("minbucket")
-ax.set_ylabel("minsplit")
-ax.set_zlabel("ganancia")
-plt.show()
-
-
+compare_surface([url,url_14],[8,14])
